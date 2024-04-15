23d0aac46cd4c5bb85690607a6a8e0930fc64dc5
minusno0708 <naoyao1014@gmail.com>
2024-04-15 23:50:06 +0900

    feat:ポート番号を引数から指定可能にする

--- src/main.erl
+++ src/main.erl
@@ -3,5 +3,12 @@
 
 -import(server, [start/1]).
 
+main([Port]) ->
+    try
+        start(list_to_integer(Port))
+    catch
+        error:Reason -> io:format("Error: ~p~n", [Reason])
+    end;
+
 main(_) ->
     server:start(8080).
