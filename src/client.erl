a45f93e258f1f60f115427515e8231be9076ada7
minusno0708 <naoyao1014@gmail.com>
2024-04-15 19:24:01 +0900

    feat:クライアントサーバ間での相互通信を実装

--- src/client.erl
+++ src/client.erl
@@ -5,4 +5,6 @@ send_msg() ->
     Port = 8080,
     {ok, Sock} = gen_tcp:connect("localhost", Port, [binary, {active, false}, {packet, 0}]),
     ok = gen_tcp:send(Sock, "Hello, world!"),
+    {ok, Data} = gen_tcp:recv(Sock, 0),
+    io:format("Received: ~p~n", [Data]),
     ok = gen_tcp:close(Sock).
--- src/server.erl
+++ src/server.erl
@@ -4,10 +4,9 @@
 start() ->
     Port = 8080,
     io:format("Start streaming server on ~p~n", [Port]),
-    case gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}]) of
+    case gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]) of
         {ok, LSock} -> 
-            loop_acceptor(LSock),
-            gen_tcp:close(LSock);
+            loop_acceptor(LSock);
         {error, Reason} -> 
             io:format("Error: ~p~n", [Reason]),
             ok
@@ -15,16 +14,14 @@ start() ->
 
 loop_acceptor(LSock) ->
     {ok, Sock} = gen_tcp:accept(LSock),
-    spawn(fun() -> handle_server(Sock) end),
+    spawn(fun() -> do_recv(Sock) end),
     loop_acceptor(LSock).
-
-handle_server(Sock) ->
-    {ok, Bin} = do_recv(Sock, []),
-    io:format("Received: ~p~n", [Bin]),
-    ok = gen_tcp:close(Sock).
     
-do_recv(Sock, Bs) ->
+do_recv(Sock) ->
     case gen_tcp:recv(Sock, 0) of
-        {ok, B} -> do_recv(Sock, [Bs, B]);
-        {error, closed} -> {ok, list_to_binary(Bs)}
+        {ok, Data} -> 
+            io:format("Received: ~p~n", [Data]),
+            gen_tcp:send(Sock, Data),
+            do_recv(Sock);
+        {error, closed} -> ok
     end.
