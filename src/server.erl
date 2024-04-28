8b869261f7368201b55651783294ff564f72d56c
minusno0708 <naoyao1014@gmail.com>
2024-04-29 01:42:17 +0900

    feat:ステータスコードからメッセージを出力する関数を作成

--- src/server.erl
+++ src/server.erl
@@ -27,25 +27,25 @@ handle_server(Sock) ->
                     Header = [
                         "Content-Type: text/plain\r\n"
                     ],
-                    send_resp(Sock, "200 OK", Header, <<"Hello, client!">>);
+                    send_resp(Sock, 200, Header, <<"Hello, client!">>);
                 [<<"GET">>, <<"/page">>, _] ->
                     {ok, File} = read_page(<<"index">>),
                     Header = [
                         "Content-Type: text/html\r\n"
                     ],
-                    send_resp(Sock, "200 OK", Header, File);
+                    send_resp(Sock, 200, Header, File);
                 [<<"GET">>, <<"/page/", PageName/binary>>, _] ->
                     case read_page(PageName) of
                         {ok, File} ->
                             Header = [
                                 "Content-Type: text/html\r\n"
                             ],
-                            send_resp(Sock, "200 OK", Header, File);
+                            send_resp(Sock, 200, Header, File);
                         {error, File} ->
                             Header = [
                                 "Content-Type: text/html\r\n"
                             ],
-                            send_resp(Sock, "404 Not Found", Header, File)
+                            send_resp(Sock, 404, Header, File)
                     end;
                 [<<"GET">>, <<"/video/", VideoName/binary>>, _] ->
                     case is_exist_video(VideoName) of
@@ -55,13 +55,13 @@ handle_server(Sock) ->
                                 "Content-Type: text/html\r\n"
                             ],
                             EmbedFile = re:replace(binary_to_list(File), "%%VIDEO_NAME%%", binary_to_list(VideoName), [{return, list}]),
-                            send_resp(Sock, "200 OK", Header, list_to_binary(EmbedFile));
+                            send_resp(Sock, 200, Header, list_to_binary(EmbedFile));
                         false ->
                             {ok, File} = read_page(<<"404">>),
                             Header = [
                                 "Content-Type: text/html\r\n"
                             ],
-                            send_resp(Sock, "404 Not Found", Header, File)
+                            send_resp(Sock, 404, Header, File)
                     end;
                 [<<"GET">>, <<"/stream/", VideoPath/binary>>, _] ->
                     case load_video(VideoPath) of
@@ -70,24 +70,24 @@ handle_server(Sock) ->
                                 "Content-Type: video/mp4\r\n",
                                 "Access-Control-Allow-Origin: *\r\n"
                             ],
-                            send_resp(Sock, "200 OK", Header, File);
+                            send_resp(Sock, 200, Header, File);
                         {segment, File} ->
                             Header = [
                                 "Content-Type: video/mp4\r\n",
                                 "Access-Control-Allow-Origin: *\r\n"
                             ],
-                            send_resp(Sock, "200 OK", Header, File);
+                            send_resp(Sock, 200, Header, File);
                         {error, _} ->
                             Header = [
                                 "Content-Type: text/plain\r\n"
                             ],
-                            send_resp(Sock, "404 Not Found", Header, <<"Not found!">>)
+                            send_resp(Sock, 404, Header, <<"Not found!">>)
                     end;
                 _ ->
                     Header = [
                         "Content-Type: text/plain\r\n"
                     ],
-                    send_resp(Sock, "404 Not Found", Header, <<"Not found!">>)
+                    send_resp(Sock, 404, Header, <<"Not found!">>)
             end,
             handle_server(Sock);
         {error, closed} -> ok
@@ -115,11 +115,18 @@ headers_to_map(HeaderList, HeaderMap) ->
             headers_to_map(Rest, HeaderMap#{Key => Value})
     end.
 
+status_msg(StatusCode) ->
+    case StatusCode of
+        200 -> "OK";
+        404 -> "Not Found";
+        _ -> "Internal Server Error"
+    end.
+
 send_resp(Sock, Status, Header, Body) ->
     ListBody = binary_to_list(Body),
     Resp = 
         lists:concat([
-        "HTTP/1.1 " ++ Status ++ " \r\n",
+        "HTTP/1.1 " ++ integer_to_list(Status) ++ status_msg(Status) ++ " \r\n",
         "Content-Length: " ++ integer_to_list(length(ListBody)) ++ "\r\n",
         Header,
         "\r\n",
