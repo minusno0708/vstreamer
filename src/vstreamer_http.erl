2ae9746cfbe8d28200c2aa3301666407cc289573
minusno0708 <naoyao1014@gmail.com>
2024-10-01 02:47:44 +0900

    refactor: リクエストのhttp解析部分を分離

--- src/vstreamer_http.erl
+++ src/vstreamer_http.erl
@@ -1,6 +1,6 @@
 -module(vstreamer_http).
 
--export([serialize_http/2, serialize_http/3]).
+-export([parse_http/1, serialize_http/2, serialize_http/3, conn_body/2, is_received/2]).
 
 status_msg(StatusCode) ->
     case StatusCode of
@@ -12,6 +12,39 @@ status_msg(StatusCode) ->
         404 -> <<"404 Not Found">>;
         _ -> <<"500 Internal Server Error">>
     end.
+
+parse_http(Req) ->
+    [HeaderSection | BodySection] = string:split(Req, "\r\n\r\n", all),
+    [StatusLine | HeaderLine] = string:split(HeaderSection, "\r\n", all),
+
+    Status = string:split(StatusLine, " ", all),
+    Header = parse_header(HeaderLine, #{}),
+    Body = conn_body(BodySection, <<>>),
+
+    {Status, Header, Body}.
+
+parse_header(HeaderList, HeaderMap) ->
+    case HeaderList of
+        [] -> HeaderMap;
+        [Header | Rest] ->
+            case string:split(Header, ": ", all) of
+                [Key, Value] -> 
+                    parse_header(Rest, HeaderMap#{Key => Value});
+                _ -> 
+                    parse_header(Rest, HeaderMap)
+            end
+    end.
+
+conn_body(BodySection, Body) ->
+    case BodySection of
+        [] -> Body;
+        [BodyHead] -> <<Body/binary, BodyHead/binary>>;
+        [BodyHead | BodyTail] ->
+            conn_body(BodyTail, <<Body/binary, BodyHead/binary, "\r\n\r\n">>)
+    end.
+
+is_received(Header, Body) ->
+    byte_size(Body) >= binary_to_integer(maps:get(<<"Content-Length">>, Header, <<"0">>)).       
 
 serialize_http(StatusCode, Header) ->
     <<
--- src/vstreamer_web.erl
+++ src/vstreamer_web.erl
@@ -3,7 +3,7 @@
 -export([run/1]).
 
 -import(vstreamer_files, [read_page/1, load_video/1, is_exist_video/1, download_video/2, get_video_list/0]).
--import(vstreamer_http, [serialize_http/2, serialize_http/3]).
+-import(vstreamer_http, [parse_http/1, serialize_http/2, serialize_http/3, conn_body/2, is_received/2]).
 -import(vstreamer_router, [router/3]).
 
 run(Port) ->
@@ -23,13 +23,13 @@ loop_acceptor(LSock) ->
     
 handle_server(Sock) ->
     case read_req(Sock) of
-        {ok, [Method, Path, _Version], _ReqHeaders, _ReqBody} ->
+        {ok, [Method, Path, _Version], _ReqHeader, _ReqBody} ->
             io:format("Request: ~p ~p~n", [Method, Path]),
             case router(Method, Path, _ReqBody) of
-                {Status, Header, File} -> 
-                    send_resp(Sock, Status, Header, File);
-                {Status, Header} ->
-                    send_resp(Sock, Status, Header)
+                {Status, RespHeader, RespBody} -> 
+                    send_resp(Sock, Status, RespHeader, RespBody);
+                {Status, RespHeader} ->
+                    send_resp(Sock, Status, RespHeader)
             end,
             handle_server(Sock);
         {error, closed} -> ok
@@ -37,50 +37,27 @@ handle_server(Sock) ->
 
 read_req(Sock) ->
     case gen_tcp:recv(Sock, 0) of
-        {ok, Data} -> 
-            [HeaderSection | BodySection] = string:split(Data, "\r\n\r\n", all),
-            [StateLine | HeaderLine] = string:split(HeaderSection, "\r\n", all),
-
-            Status = string:split(StateLine, " ", all),
-            Header = headers_to_map(HeaderLine, #{}),
-            Body = body_conn(BodySection, <<>>),
+        {ok, Req} -> 
+            io:format("Received: ~p~n", [Req]),
             
-            case byte_size(Body) >=  binary_to_integer(maps:get(<<"Content-Length">>, Header, <<"0">>)) of
+            {Status, Header, Body} = parse_http(Req),
+
+            case is_received(Header, Body) of
                 true -> {ok, Status, Header, Body};
                 false -> continue_recv(Sock, Status, Header, Body)
             end;
         {error, closed} -> {error, closed}
     end.
 
-continue_recv(Sock, Status, Header, PreBody) ->
+continue_recv(Sock, Status, Header, ReceivedBody) ->
     case gen_tcp:recv(Sock, 0) of
-        {ok, Data} ->
-            Body = body_conn([Data], PreBody),
-            case byte_size(Body) >= binary_to_integer(maps:get(<<"Content-Length">>, Header, <<"0">>)) of
+        {ok, Req} ->
+            Body = conn_body([Req], ReceivedBody),
+            case is_received(Header, Body) of
                 true -> {ok, Status, Header, Body};
                 false -> continue_recv(Sock, Status, Header, Body)
             end;
         {error, closed} -> {error, closed}
-    end.
-
-headers_to_map(HeaderList, HeaderMap) ->
-    case HeaderList of
-        [] -> HeaderMap;
-        [Header | Rest] ->
-            case string:split(Header, ": ", all) of
-                [Key, Value] -> 
-                    headers_to_map(Rest, HeaderMap#{Key => Value});
-                _ -> 
-                    headers_to_map(Rest, HeaderMap)
-            end
-    end.
-
-body_conn(BodySection, Body) ->
-    case BodySection of
-        [] -> Body;
-        [BodyHead] -> <<Body/binary, BodyHead/binary>>;
-        [BodyHead | BodyTail] ->
-            body_conn(BodyTail, <<Body/binary, BodyHead/binary, "\r\n\r\n">>)
     end.
 
 send_resp(Sock, Status, Header) ->
