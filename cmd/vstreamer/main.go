42c13767fb19cb738a790832b8803db3b9139ec0
minusno0708 <naoyao1014@gmail.com>
2025-04-07 23:36:29 +0900

    feat:endpointを作成

--- cmd/vstreamer/main.go
+++ cmd/vstreamer/main.go
@@ -4,11 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, world!")
+	})
+
+	http.HandleFunc("/pages/", func(w http.ResponseWriter, r *http.Request) {
+		pageName := strings.TrimPrefix(r.URL.Path, "/page/")
+		fmt.Fprintf(w, "show %s page", pageName)
+	})
+
+	http.HandleFunc("/streams/", func(w http.ResponseWriter, r *http.Request) {
+		videoPath := strings.TrimPrefix(r.URL.Path, "/stream/")
+		fmt.Fprintf(w, "stream %s video", videoPath)
+	})
+
+	http.HandleFunc("/videos/", func(w http.ResponseWriter, r *http.Request) {
+		videoId := strings.TrimPrefix(r.URL.Path, "/videos/")
+		fmt.Fprintf(w, "show %s video", videoId)
+	})
+
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "upload video")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
