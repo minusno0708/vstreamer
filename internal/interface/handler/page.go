ea70f929b0a04d153fda3f1b521dfc410d52b9f5
minusno0708 <naoyao1014@gmail.com>
2025-04-10 00:29:34 +0900

    feat:指定したHTMLファイルをレスポンス

--- internal/interface/handler/page.go
+++ internal/interface/handler/page.go
@@ -1,12 +1,11 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func PageHandler(c echo.Context) error {
 	pageName := c.Param("name")
-	return c.String(http.StatusOK, "show "+pageName+" page")
+
+	return c.File("pages/" + pageName + ".html")
 }
