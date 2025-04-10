0ae990c2f7ad0bead700b574e1de0eb8616c47dd
minusno0708 <naoyao1014@gmail.com>
2025-04-10 21:39:38 +0900

    feat:ページが存在しない場合NotFoundページを表示

--- internal/interface/handler/page.go
+++ internal/interface/handler/page.go
@@ -1,11 +1,32 @@
 package handler
 
 import (
+	"os"
+
 	"github.com/labstack/echo/v4"
 )
+
+const (
+	pagesDir = "pages/"
+)
+
+func toPagePath(pageName string) string {
+	return pagesDir + pageName + ".html"
+}
+
+func isPageExist(path string) bool {
+	_, err := os.Stat(toPagePath(path))
+	return err == nil
+}
 
 func PageHandler(c echo.Context) error {
 	pageName := c.Param("name")
 
-	return c.File("pages/" + pageName + ".html")
+	pagePath := toPagePath(pageName)
+
+	if !isPageExist(pagePath) {
+		pagePath = toPagePath("404")
+	}
+
+	return c.File(pagePath)
 }
