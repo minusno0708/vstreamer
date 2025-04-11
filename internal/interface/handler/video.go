b836c0261cf2b51c484754f0bf4092101c854f3d
minusno0708 <naoyao1014@gmail.com>
2025-04-12 01:33:58 +0900

    refactor:動画ファイルの保存部分を別関数に分割

--- internal/interface/handler/video.go
+++ internal/interface/handler/video.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 
@@ -16,19 +17,10 @@ func toVideoPath(videoName string) string {
 	return videoDir + videoName
 }
 
-func VideoGetHandler(c echo.Context) error {
-	pageName := c.Param("id")
-	return c.String(http.StatusOK, "show "+pageName+" video")
-}
-
-func VideoUploadHandler(c echo.Context) error {
-	videoFile, err := c.FormFile("video")
-	if err != nil {
-		return c.String(http.StatusBadRequest, "video not found")
-	}
+func saveVideoFile(videoFile *multipart.FileHeader) error {
 	src, err := videoFile.Open()
 	if err != nil {
-		return c.String(http.StatusInternalServerError, "file open error")
+		return err
 	}
 	defer src.Close()
 
@@ -40,6 +32,24 @@ func VideoUploadHandler(c echo.Context) error {
 
 	if _, err = io.Copy(dst, src); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func VideoGetHandler(c echo.Context) error {
+	pageName := c.Param("id")
+	return c.String(http.StatusOK, "show "+pageName+" video")
+}
+
+func VideoUploadHandler(c echo.Context) error {
+	videoFile, err := c.FormFile("video")
+	if err != nil {
+		return c.String(http.StatusBadRequest, "video not found")
+	}
+
+	if err := saveVideoFile(videoFile); err != nil {
+		return c.String(http.StatusInternalServerError, "failed to save video")
 	}
 
 	return c.String(http.StatusOK, "upload video")
