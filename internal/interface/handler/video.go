c4babdcdca02dfeb04e820bffa0ba6f381f9aca3
minusno0708 <naoyao1014@gmail.com>
2025-04-12 01:18:01 +0900

    feat:uploadされたビデオを保存

--- internal/interface/handler/video.go
+++ internal/interface/handler/video.go
@@ -1,10 +1,20 @@
 package handler
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
+
+const (
+	videoDir = "videos/"
+)
+
+func toVideoPath(videoName string) string {
+	return videoDir + videoName
+}
 
 func VideoGetHandler(c echo.Context) error {
 	pageName := c.Param("id")
@@ -12,5 +22,25 @@ func VideoGetHandler(c echo.Context) error {
 }
 
 func VideoUploadHandler(c echo.Context) error {
+	videoFile, err := c.FormFile("video")
+	if err != nil {
+		return c.String(http.StatusBadRequest, "video not found")
+	}
+	src, err := videoFile.Open()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "file open error")
+	}
+	defer src.Close()
+
+	dst, err := os.Create(toVideoPath(videoFile.Filename))
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
 	return c.String(http.StatusOK, "upload video")
 }
