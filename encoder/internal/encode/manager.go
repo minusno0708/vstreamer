7960dabccebc0cd8860effc412b2509f5850a6b4
minusno0708 <naoyao1014@gmail.com>
2025-05-21 00:50:20 +0900

    feat: ffmpegコマンドを生成するメソッドを作成

--- encoder/internal/encode/manager.go
+++ encoder/internal/encode/manager.go
@@ -1,26 +1,13 @@
 package encode
 
 import (
-	"os/exec"
+	"github.com/minusno0708/vstreamer/encoder/internal/encode/ffmpeg"
 )
 
 func Encode(basePath, encodeDir string) error {
-	cmd := exec.Command(
-		"ffmpeg",
-		"-i", basePath,
-		"-c:v", "libx264",
-		"-b:v", "1M",
-		"-s", "1280x720",
-		"-keyint_min", "150",
-		"-g", "150",
-		"-profile:v", "high",
-		"-preset", "medium",
-		"-c:a", "aac",
-		"-ac", "2",
-		"-b:a", "128k",
-		"-f", "dash",
-		encodeDir+"/manifest.mpd",
-	)
+	cmdBuilder := ffmpeg.NewCmdBuilder(basePath, encodeDir)
+
+	cmd := cmdBuilder.Build()
 
 	_, err := cmd.Output()
 	if err != nil {
