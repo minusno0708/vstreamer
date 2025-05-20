b82313ee37f3e7eeb55e3132f8f2cf8406c2c3c5
minusno0708 <naoyao1014@gmail.com>
2025-05-20 21:42:13 +0900

    chore: encoderをDockerコンテナ上で起動

--- encoder/cmd/encoder/main.go
+++ encoder/cmd/encoder/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/minusno0708/vstreamer/encoder/internal/rabbitmq"
 )
@@ -9,7 +11,15 @@ import (
 func main() {
 	log.Println("Starting video encoder...")
 
-	conn, err := rabbitmq.NewConnection("amqp://guest:guest@localhost:5672/")
+	mqUser := os.Getenv("MQ_USER")
+	mqPass := os.Getenv("MQ_PASS")
+	mqHost := os.Getenv("MQ_HOST")
+	mqPort := os.Getenv("MQ_PORT")
+	queueName := os.Getenv("QUEUE_NAME")
+
+	dsn := fmt.Sprintf("amqp://%s:%s@%s:%s/", mqUser, mqPass, mqHost, mqPort)
+
+	conn, err := rabbitmq.NewConnection(dsn)
 	if err != nil {
 		log.Panicf("Failed to connect to RabbitMQ: %v\n", err)
 		return
@@ -23,7 +33,7 @@ func main() {
 	}
 	defer ch.Close()
 
-	msgs, err := rabbitmq.Receive(ch, "video-encoder")
+	msgs, err := rabbitmq.Receive(ch, queueName)
 	if err != nil {
 		log.Panicf("Failed to receive message: %v\n", err)
 		return
